d8f3e712f9c44548f124046ec671212c18a59903
Abhiram <sriramoji.abhi@gmail.com>
2024-02-28 13:39:21 +0530

    new changes

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -5,6 +5,15 @@ export const Weather = (props) => {
   return (
     <>
       <div className=" cursor-default container mx-auto bg-white/20 items flex flex-col justify-center items-center rounded-md">
+        <div className="flex gap-2 items-center justify-center mx-3">
+          <input
+            type="text"
+            name="city"
+            placeholder="Enter City name"
+            className="w-full p-2 rounded-lg mt-3  place-holder-center"
+          />
+          <button>searach</button>
+        </div>
         <div className="flex flex-col gap-1 items-center">
           <img src={props.icon} alt="Weather Icon" className="mt-5 p-0.5" />{" "}
           <p className="font-regular p-1 text-[48px] m-0">
